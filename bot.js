ae811aef3020c8b6d47cfe5221288902206f621e
OfficialMe55 <85696205+OfficialMe55@users.noreply.github.com>
2021-10-30 12:36:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -52,10 +52,7 @@ client.on('message', async (msg) => {
 	if(msg.content.toLowerCase().includes('tele'.toLowerCase()) && msg.guild.id == '455086148105142329'){
 	msg.channel.send('<@275658656727695364>');
 }
-	if(msg.content.toLowerCase().includes('króli'.toLowerCase()) && msg.guild.id == '455086148105142329' && !msg.content.toLowerCase().includes('króli '.toLowerCase())){
-	msg.channel.send('<@754315545113002025>');
-}
-	if(msg.content.toLowerCase().includes('królicz'.toLowerCase()) && msg.guild.id == '455086148105142329'){
+	if(msg.content.toLowerCase().includes('króli'.toLowerCase()) && msg.guild.id == '455086148105142329' && !msg.content.toLowerCase().includes('króli '.toLowerCase()) && !msg.content.toLowerCase().includes(' króli'.toLowerCase())){
 	msg.channel.send('<@754315545113002025>');
 }
 
